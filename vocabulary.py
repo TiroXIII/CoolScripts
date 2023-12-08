976c5a286c47c880c772a45e84d5dc1830974f55
tiroxiii <tpdissanayake13@gmail.com>
2023-12-08 21:50:26 +0000

    Added vocabulary.py to CoolScripts

---
+++ vocabulary.py
@@ -0,0 +1,54 @@
+"""Functions for creating, transforming, and adding prefixes to strings."""
+"""Vocabulary exercise"""
+
+import re
+
+"""One of the most common prefixes in English is un, meaning "not". In this activity, your sister needs to make negative, or "not" words by adding un to them.
+
+Implement the add_prefix_un(<word>) function that takes word as a parameter and returns a new un prefixed word:"""
+def add_prefix_un(word):
+    function = "un" + word
+    return function
+
+"""There are four more common prefixes that your sister's class is studying: en (meaning to 'put into' or 'cover with'), pre (meaning 'before' or 'forward'), auto (meaning 'self' or 'same'), and inter (meaning 'between' or 'among').
+
+In this exercise, the class is creating groups of vocabulary words using these prefixes, so they can be studied together. Each prefix comes in a list with common words it's used with. The students need to apply the prefix and produce a string that shows the prefix applied to all of the words.
+
+Implement the make_word_groups(<vocab_words>) function that takes a vocab_words as a parameter in the following form: [<prefix>, <word_1>, <word_2> .... <word_n>], and returns a string with the prefix applied to each word that looks like: '<prefix> :: <prefix><word_1> :: <prefix><word_2> :: <prefix><word_n>'"""
+def make_word_groups(vocab_words):
+    new_list = [vocab_words[0]]
+    for words in vocab_words[1:]:
+        new = vocab_words[0] + words
+        new_list.append(new)
+    return " :: ".join(new_list)
+
+"""ness is a common suffix that means 'state of being'. In this activity, your sister needs to find the original root word by removing the ness suffix. But of course there are pesky spelling rules: If the root word originally ended in a consonant followed by a 'y', then the 'y' was changed to 'i'. Removing 'ness' needs to restore the 'y' in those root words. e.g. happiness --> happi --> happy.
+
+Implement the remove_suffix_ness(<word>) function that takes in a word, and returns the root word without the ness suffix.
+
+"""
+def remove_suffix_ness(word):
+    if "ness" in word:
+        new_word = word[:-4]
+        if new_word[-1] == "i":
+            newer_word = list(new_word[:-1])
+            newer_word += "y"
+            return "".join(newer_word)
+        return new_word
+    return False
+
+"""Suffixes are often used to change the part of speech a word is assigned to. A common practice in English is "verbing" or "verbifying" -- where an adjective becomes a verb by adding an en suffix.
+
+In this task, your sister is going to practice "verbing" words by extracting an adjective from a sentence and turning it into a verb. Fortunately, all the words that need to be transformed here are "regular" - they don't need spelling changes to add the suffix.
+
+Implement the adjective_to_verb(<sentence>, <index>) function that takes two parameters. A sentence using the vocabulary word, and the index of the word, once that sentence is split apart. The function should return the extracted adjective as a verb."""
+def adjective_to_verb(sentence, index):
+    new_sentence = sentence.split()
+    word = new_sentence[index]
+    letter = list(word)
+    if index == -1 and re.match(r"[.?!]", letter[-1]):
+        letter = letter[:-1]
+        letter.append("en")
+        return "".join(letter)
+    letter.append("en")
+    return "".join(letter)
