551d04dd38d9fddf870a10c4562f46a0eaa2d11a
tiroxiii <tpdissanayake13@gmail.com>
2023-12-19 13:01:23 +0000

    Removed the unnecessary for loop and added a arithmetic function to find ohms to reduce lines of code

--- raspberry_pi_coding.py
+++ raspberry_pi_coding.py
@@ -1,17 +1,6 @@
 def label(colors):
-    colors_list = ['black','brown','red','orange','yellow','green','blue','violet','grey','white']
-    reading = []
-    counter = 1
-    for color in colors:
-        if counter < 3:
-            reading.append(str(colors_list.index(color)))
-        if counter == 3:
-            x = 10**colors_list.index(color)
-            reading.append((x))
-            break
-        counter += 1
-    y = int(''.join(reading[0:2]))
-    value = x*y
+    colors_list = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+    ohms = (((10 * colors_list.index(colors[0])) + colors_list.index(colors[1])) * 10 ** colors_list.index(colors[2]))
     if value/1000 < 1:
         return str(value) + ' ohms'
     if 1 <= value/1000 <= 1000:
