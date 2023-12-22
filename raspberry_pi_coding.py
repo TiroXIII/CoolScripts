98793662e0876fb0bb6233b418a41e0833a34927
tiroxiii <tpdissanayake13@gmail.com>
2023-12-22 10:45:52 +0000

    changed variable name from value to ohms

--- raspberry_pi_coding.py
+++ raspberry_pi_coding.py
@@ -1,11 +1,11 @@
 def label(colors):
     colors_list = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
     ohms = (((10 * colors_list.index(colors[0])) + colors_list.index(colors[1])) * 10 ** colors_list.index(colors[2]))
-    if value/1000 < 1:
-        return str(value) + ' ohms'
-    if 1 <= value/1000 <= 1000:
-        return str(int(value/1000)) + ' kiloohms'
-    if 1 <= value/1000000 <= 1000:
-        return str(int(value/1000000)) + ' megaohms'
-    if 1 <= value/1000000000 <= 1000:
-        return str(int(value/1000000000)) + ' gigaohms'
+    if ohms / 1000 < 1:
+        return str(ohms) + ' ohms'
+    if 1 <= ohms / 1000 <= 1000:
+        return str(int(ohms / 1000)) + ' kiloohms'
+    if 1 <= ohms / 1000000 <= 1000:
+        return str(int(ohms / 1000000)) + ' megaohms'
+    if 1 <= ohms / 1000000000 <= 1000:
+        return str(int(ohms / 1000000000)) + ' gigaohms'
