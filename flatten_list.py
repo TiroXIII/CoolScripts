268ac1784fc403b8a41860b2cc7de3e29a2d5ef6
tiroxiii <tpdissanayake13@gmail.com>
2023-12-24 13:28:38 +0000

    Added flatten_list.py to CoolScripts

---
+++ flatten_list.py
@@ -0,0 +1,10 @@
+# This function flattens a list using recursion
+
+def flatten(iterable):
+    flattened = []
+    for item in iterable:
+        if isinstance(item, list):
+            flattened.extend(flatten(item))
+        elif item is not None:
+            flattened.append(item)
+    return flattened
